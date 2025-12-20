f2b80ba8563079f7c3e5653c4da3e63e018e6040
金崎悠翔 <146085950+Yuto-Kanasaki@users.noreply.github.com>
2025-12-20 18:20:28 +0900

    タイピングゲーム完成

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// const content = document.getElementById("main");
+// console.log("getElementById", content);
+
+// const data = document.getElementsByClassName("data");
+// console.log("getElementsByClassName", data);
+// console.log(data[0])
+
+// const h1 = document.querySelector(".wrapper h1");
+// console.log("querySelector", h1);
+
+// const data2 = document.querySelectorAll(".unordered-list li");
+// console.log("querySelectorAll", data2);
+
+// const element = document.createElement("p");
+
+// const sample1 = document.querySelector(".list1");
+// console.log("textContent before", sample1.textContent);
+// sample1.textContent = "最初のデータ";
+// console.log("textContent after", sample1.textContent);
+// this.location.reload()
